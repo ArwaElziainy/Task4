a0abffe6681b413cc6c6379bdee065131f498a5a
ArwaElziainy <q.m11@icloud.com>
2024-10-19 14:58:15 +0300

    19/10

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -3,8 +3,11 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/odata/v2/ODataModel",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function(Controller, JSONModel, ODataModel, Filter, FilterOperator) {
+	"sap/ui/model/FilterOperator",
+	"sap/ui/export/Spreadsheet",
+	"sap/m/MessageToast",
+	"sap/ui/core/format/DateFormat"
+], function(Controller, JSONModel, ODataModel, Filter, FilterOperator, Spreadsheet, MessageToast, DateFormat) {
 	"use strict";
 
 	return Controller.extend("Task4.controller.View1", {
@@ -20,6 +23,7 @@ sap.ui.define([
 
 			// Set the model to the view
 			this.getView().setModel(oModel, "odataModel");
+			this._loadAllRecords();
 
 		},
 
@@ -116,15 +120,15 @@ sap.ui.define([
 			// });
 
 		},
-		onFilterItems:function(){
-			
+		onFilterItems: function() {
+
 			var table = "table";
-			var scannerField = this.getView().byId("idScannerInput").getValue();
+			var scannerField = this.getView().byId("salesDocInput").getValue();
 			this.filter(scannerField, table);
 		},
 		filter: function(formalData, table) {
 			// var sQuery = this.byId("inputVbeln").getValue();
-			
+
 			//	if (formalData.length > 0) {
 			var oFilter = new Filter({
 				filters: [
@@ -137,85 +141,6 @@ sap.ui.define([
 			var oTable = this.byId(table),
 				oBinding = oTable.getBinding("items");
 			oBinding.filter(oFilter);
-			
-			
-			
-			// var sQuery = this.byId("inputVbeln").getValue();
-
-			// if (sQuery) {
-			// 	var sPath = "/SOHeadSet('" + sQuery + "')";
-			// 	var oModel = this.getView().getModel("odataModel");
-			// 	if (!oModel) {
-			// 		console.error("OData model not found");
-			// 		return;
-			// 	}
-			// 	var oTable = this.byId("table");
-
-			// 	// Unbind previous items
-			// 	oTable.unbindItems();
-
-			// 	// Fetch specific entry based on sQuery
-			// 	oModel.read(sPath, {
-					
-			// 		success: function(oData) {
-			// 			var aData = [oData];
-			// 			var oLocalModel = new JSONModel({
-			// 				results: aData
-			// 			});
-			// 			oTable.setModel(oLocalModel, "localModel");
-
-			// 			oTable.bindItems({
-			// 				path: "localModel>/results",
-			// 				template: new sap.m.ColumnListItem({
-			// 					cells: [
-			// 					new sap.m.CheckBox({
-			// 						selected: "{localModel>Selected}",
-			// 						select: this.onItemSelect.bind(this)
-			// 					}),
-			// 					new sap.m.ObjectIdentifier({
-			// 						title: "{localModel>Vbeln}"
-			// 					}),
-			// 					new sap.m.Text({
-			// 						text: "{localModel>Erdat}"
-			// 					}),
-			// 					new sap.m.Text({
-			// 						text: "{localModel>Vprgr}"
-			// 					}),
-			// 					new sap.m.Text({
-			// 						text: "{localModel>Ernam}"
-			// 					}),
-			// 					new sap.m.Text({
-			// 						text: "{localModel>Auart}"
-			// 					}),
-			// 					new sap.m.Text({
-			// 						text: "{localModel>Netwr}"
-			// 					}),
-			// 					new sap.m.Text({
-			// 						text: "{localModel>Waerk}"
-			// 					}),
-			// 					new sap.m.Text({
-			// 						text: "{localModel>Vkorg}"
-			// 					}),
-			// 					new sap.m.Text({
-			// 						text: "{localModel>Gwldt}"
-			// 					}),
-			// 					new sap.m.Text({
-			// 						text: "{localModel>Kunnr}"
-			// 					})
-			// 				],
-			// 					type: "Navigation",
-			// 					press: this.onPress.bind(this)
-			// 				})
-			// 			});
-			// 		}.bind(this),
-			// 		error: function(oError) {
-			// 			console.error("Error occurred: ", oError);
-			// 		}
-			// 	});
-
-			// } else {
-			// 	oTable.unbindItems();
-			// }
 
 		},
 		onPress: function(oEvent) {
@@ -269,7 +194,85 @@ sap.ui.define([
 			// Enable or disable the Delete button based on selection
 			this._updateDeleteButtonState(bAnySelected);
 		},
+		onOpenSortDialog: function() {
+			if (!this._oSortDialog) {
+				this._oSortDialog = this.byId("sortDialog");
+			}
+			this._oSortDialog.open();
+		},
 
+		onCloseSortDialog: function() {
+			this._oSortDialog.close();
+		},
+
+		onSort: function() {
+			var oSelect = this.byId("columnSelect");
+			var sSelectedColumn = oSelect.getSelectedKey();
+
+			var oSortOrderGroup = this.byId("sortOrderGroup");
+			var bDescending = oSortOrderGroup.getSelectedButton().getText() === "Descending";
+
+			var oTable = this.byId("table");
+			var oBinding = oTable.getBinding("items");
+			var oSorter = new sap.ui.model.Sorter(sSelectedColumn, bDescending);
+			oBinding.sort(oSorter);
+
+			this._oSortDialog.close();
+		},
+		onExport: function() {
+			var oTable = this.byId("table");
+			var oBinding = oTable.getBinding("items");
+			// var oModel = oBinding.getModel("odataModel");
+			var aCols = this.createColumnConfig();
+			var oSettings = {
+				workbook: {
+					columns: aCols
+				},
+				dataSource: oBinding,
+				fileName: "TableExport.xlsx"
+			};
+			var oSheet = new Spreadsheet(oSettings);
+			oSheet.build().finally(function() {
+				oSheet.destroy();
+			});
+		},
+
+		createColumnConfig: function() {
+			return [{
+					label: 'Sales Document',
+					property: 'Vbeln'
+				}, {
+					label: 'Created on',
+					property: 'Erdat'
+				}
+				// Add more columns as needed
+			];
+		},
+		onOpenFilterDialog: function() {
+			if (!this._oDialog) {
+				this._oDialog = this.byId("filterDialog");
+			}
+			this._oDialog.open();
+		},
+		onFilterDialogConfirm: function() {
+			var sSelectedColumn = this.byId("filterColumnSelect").getSelectedKey();
+			var sValue = this.byId("filterValueInput").getValue();
+
+			var aFilters = [];
+			if (sValue) {
+				aFilters.push(new Filter(sSelectedColumn, FilterOperator.Contains, sValue));
+			}
+
+			var oTable = this.byId("table");
+			var oBinding = oTable.getBinding("items");
+			oBinding.filter(aFilters);
+
+			this._oDialog.close();
+		},
+
+		onCloseFilterDialog: function() {
+			this.byId("filterDialog").close();
+		},
 		// Update the Delete button state based on checkbox selections
 		_updateDeleteButtonState: function() {
 			var aItems = this.oTable.getItems();
@@ -278,6 +281,195 @@ sap.ui.define([
 			});
 
 			this.oDeleteButton.setEnabled(bAnySelected);
+		},
+		_loadAllRecords: function() {
+			var oModel = this.getView().getModel("odataModel");
+			var oTable = sap.ui.getCore().byId("salesDocTable");
+
+			oModel.read("/SOHeadSet", {
+				success: function(oData) {
+					console.log("All records loaded: ", oData);
+					var oJSONModel = new sap.ui.model.json.JSONModel();
+					oJSONModel.setData({
+						SOHeadSet: oData.results
+					});
+					// oTable.setModel(oJSONModel);
+				},
+				error: function(oError) {
+					console.error("Error loading records: ", oError);
+					sap.m.MessageToast.show("Error loading records.");
+				}
+			});
+		},
+		onSalesDocumentValueHelp: function(oEvent) {
+			if (!this._oValueHelpDialog) {
+				this._oValueHelpDialog = sap.ui.xmlfragment("Task4.view.ValueHelpDialog", this);
+				this.getView().addDependent(this._oValueHelpDialog);
+			}
+			this._oInput = oEvent.getSource();
+			this._oValueHelpDialog.open();
+		},
+		// Function to handle search in the Value Help Dialog
+		onValueHelpSearch: function(oEvent) {
+			var sQuery = oEvent.getParameter("newValue");
+			var aFilters = [];
+
+			if (sQuery) {
+				aFilters.push(new Filter("Vbeln", FilterOperator.Contains, sQuery));
+			}
+
+			var oTable = sap.ui.getCore().byId("salesDocTable");
+			var oBinding = oTable.getBinding("items");
+
+			// Error handling when applying filters
+			oBinding.filter(aFilters, "Application").attachRequestFailed(function(oError) {
+				MessageToast.show("Error retrieving sales documents: " + oError.getParameter("message"));
+			});
+		},
+		// Function to handle selecting an item in the table
+		onValueHelpSelect: function(oEvent) {
+			var oSelectedItem = oEvent.getParameter("listItem");
+			if (oSelectedItem) {
+				var sSelectedSalesDoc = oSelectedItem.getCells()[0].getText(); // Get Sales Document
+				this._oInput.setValue(sSelectedSalesDoc); // Set value in the input field
+				this._oValueHelpDialog.close();
+			} else {
+				MessageToast.show("Please select a Sales Document.");
+			}
+		},
+		// Function to close the Value Help Dialog
+		onValueHelpDialogClose: function() {
+			this._oValueHelpDialog.close();
+		},
+		onApplyFilters: function() {
+			var aFilters = [];
+
+			// Get the input values
+			var sCreatedOn = sap.ui.getCore().byId("createdOnInput").getDateValue();
+			var sCreatedBy = sap.ui.getCore().byId("createdByInput").getValue();
+			var sSalesDocType = sap.ui.getCore().byId("salesDocTypeInput").getValue();
+			var sCustomer = sap.ui.getCore().byId("customerInput").getValue();
+
+			// Format date to match OData service format (yyyy-MM-dd)
+			if (sCreatedOn) {
+				var oDateFormat = sap.ui.core.format.DateFormat.getDateInstance({
+					pattern: "yyyy-MM-dd"
+				});
+				var sFormattedDate = oDateFormat.format(sCreatedOn);
+				aFilters.push(new Filter("Erdat", FilterOperator.EQ, sFormattedDate));
+			}
+
+			// Add Created By filter
+			if (sCreatedBy) {
+				aFilters.push(new Filter("Ernam", FilterOperator.EQ, sCreatedBy));
+			}
+
+			// Add Sales Document Type filter
+			if (sSalesDocType) {
+				aFilters.push(new Filter("Auart", FilterOperator.EQ, sSalesDocType));
+			}
+
+			// Add Customer filter
+			if (sCustomer) {
+				aFilters.push(new Filter("Kunnr", FilterOperator.EQ, sCustomer));
+			}
+
+			console.log("Filters to apply: ", aFilters);
+
+			// Get the OData model from the view
+			this.getView().getModel("odataModel");
+			// var oTable = sap.ui.getCore().byId("salesDocTable");
+
+			// Apply filters to the table binding
+
+			var oFilter = new Filter({
+				filters: [
+					//	new Filter("Arktx", FilterOperator.Contains, formalData),
+					new Filter("Erdat", FilterOperator.EQ, sFormattedDate),
+					new Filter("Ernam", FilterOperator.EQ, sCreatedBy),
+					new Filter("Auart", FilterOperator.EQ, sSalesDocType),
+					new Filter("Kunnr", FilterOperator.EQ, sCustomer)
+				],
+				and: false
+			});
+			//	}
+			console.log(oFilter)
+			var oTable = sap.ui.getCore().byId("salesDocTable"),
+				oBinding = oTable.getBinding("items");
+			oBinding.filter(oFilter);
+			console.log(oBinding)
+			// var oBinding = oTable.getBinding("items");
+			// if (oBinding) {
+			//     oBinding.filter(aFilters);
+			//     console.log(oBinding.filter(aFilters))
+			// } else {
+			//     console.error("No binding found for the table items.");
+			// }
+
+		},
+		onDateValidation: function(oEvent) {
+			var bValid = oEvent.getParameter("valid");
+			if (!bValid) {
+				MessageToast.show("Invalid date format. Please enter a valid date.");
+				// Optionally, reset the date value if needed
+				oEvent.getSource().setValue("");
+			}
 		}
+
+		// onValueHelpRequest: function() {
+		// 	if (!this._oValueHelpDialog) {
+		// 		this._oValueHelpDialog = new sap.m.SelectDialog("Dialog", {
+		// 			title: "Select Sales Document",
+		// 			liveChange: this._onValueHelpSearch.bind(this),
+		// 			confirm: this._onValueHelpClose.bind(this),
+		// 			cancel: this._onValueHelpClose.bind(this)
+		// 		});
+
+		// 		this.getView().addDependent(this._oValueHelpDialog);
+		// 	}
+
+		// 	// Bind the data to the dialog
+		// 	this._oValueHelpDialog.bindAggregation("items", {
+		// 		path: "odataModel>/SOHeadSet",
+		// 		template: new sap.m.StandardListItem({
+		// 			title: "{odataModel>Vbeln}",
+		// 			description: "{odataModel>Auart}"
+		// 		},
+		// 		{
+		// 			title: "{odataModel>Auart}",
+		// 			description: "{odataModel>Auart}"
+		// 		})
+
+		// 	});
+
+		// 	this._oValueHelpDialog.open();
+		// },
+		// _onValueHelpSearch: function(oEvent) {
+		// 	// debugger;
+		// 	var sValue = oEvent.getParameter("value");
+		// 	console.log("Search value:", sValue); // Log the search value
+
+		// 	var oFilter = new Filter("Vbeln", FilterOperator.StartsWith, sValue);
+		// 	console.log("Filter created:", oFilter); // Log the filter
+
+		// 	var oBinding = oEvent.getSource().getBinding("items");
+		// 	console.log("Binding found:", oBinding); // Log the binding
+
+		// 	if (oBinding) {
+		// 		oBinding.filter([oFilter]);
+		// 		console.log("Filter applied"); // Log filter application
+		// 	} else {
+		// 		console.error("Binding not found for the items aggregation.");
+		// 	}
+
+		// },
+
+		// _onValueHelpClose: function(oEvent) {
+		// 	var oSelectedItem = oEvent.getParameter("selectedItem");
+		// 	if (oSelectedItem) {
+		// 		this.byId("salesDocInput").setValue(oSelectedItem.getTitle());
+		// 	}
+		// 	oEvent.getSource().getBinding("items").filter([]);
+		// }
 	});
 });
